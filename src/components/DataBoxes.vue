0983548720dc184202edda975f3041ccfb9ea622
FlorentinaDili <73610425+FlorentinaDili@users.noreply.github.com>
2022-11-08 14:26:36 +0100

    commit

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <!-- 1st Box-->
+        <div class="shadow-md bg-green-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-red-900 font-bold mb-4">Cases</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New: </span>
+                <!-- newConfirmed comes from API -->
+                {{ numberWithCommas(stats.NewConfirmed) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total: </span>
+                <!-- totalConfirmed comes from API -->
+                {{  numberWithCommas(stats.TotalConfirmed) }}
+            </div>
+        </div>
+        <!-- 2nd Box -->
+        <div class="shadow-md bg-green-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-red-900 font-bold mb-4">Deaths</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New: </span>
+                <!-- newDeaths comes from API -->
+                {{ numberWithCommas(stats.NewDeaths) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total: </span>
+                <!-- totalDeaths comes from API -->
+                {{ numberWithCommas(stats.TotalDeaths) }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DataBoxes',
+  props: ['stats'],
+  setup () {
+    return {
+      numberWithCommas (x) {
+        return x.toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+      }
+    };
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-center bg-green-500 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fas fa-virus"></i>Covid-19 Tracker
+    </div>
+    <p>API by <a class="text-red-500" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+  /*props: {
+    msg: String
+  }*/
+}
+</script>
+
